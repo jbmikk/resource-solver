2cc4d3e7803d30a66ad5190b5c0e347aacbdffa7
Jens Mikkelsen <jbmikk@gmail.com>
2015-04-28 23:53:45 -0300

    Add resourceSolver provider and basic resource solving

--- src/resourceSolver.js
+++ src/resourceSolver.js
@@ -1,8 +1,27 @@
 angular.module('resource.solver', [])
-.constant('rs', function(conf) {
+.provider('resourceSolver', function ResourceSolver() {
+  var baseUrl = '';
 
-  function Solver($resource) {
+  this.setBaseUrl = function(url) {
+    baseUrl = url;
   };
 
-  return ['$resource', Solver];
+  this.$get = function() {
+    return {
+      getBaseUrl: function() {
+        return baseUrl;
+      }
+    };
+  };
+})
+.constant('rs', function(res) {
+
+  function Solver($resource, $resourceSolver, $state) {
+    var baseUrl = $resourceSolver.getBaseUrl();
+    var action = res.action || 'get';
+
+    return $resource(baseUrl+res.url, $state.params)[action]().$promise;
+  };
+
+  return ['$resource', '$resourceSolver', '$state', Solver];
 });
