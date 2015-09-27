23e44e98e349ca6ce8b547c16c182d5b1c97fc05
Jens Mikkelsen <jbmikk@gmail.com>
2015-09-27 15:22:54 -0300

    Add package.json, bower.json and Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true
+        }
+      }
+    },
+    karma: {
+      options: {
+        files: ['bower_components/angular/angular.js', 'src/**/*.js', 'test/resource-solver.js'],
+      },
+      unit: {
+        frameworks: [ 'jasmine' ],
+        plugins: [ 'karma-jasmine', 'karma-phantomjs-launcher' ],
+        browsers: ['PhantomJS'],
+        logLevel: 'ERROR'
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint', 'karma:unit']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-karma');
+
+  grunt.registerTask('default', ['jshint', 'karma:unit']);
+
+};
---
+++ test/resource-solver.js
@@ -0,0 +1,9 @@
+describe( 'resource-solver', function() {
+
+  it( 'resource solver', function() {
+
+    expect(true).toBe(true);
+
+  });
+
+});
